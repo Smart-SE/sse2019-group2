1f5b81d0046f504ec444f66f808a37c654eb19fc
y2klink <y2klink@gmail.com>
2019-06-25 19:30:18 +0900

    スコアリングモジュールのインターフェースとなるファイルをpush

---
+++ scoring/scoring_if.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+
+#parameter
+y_flg = True
+
+
+#main
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-B')   
+parser.add_argument('-f')
+args = parser.parse_args()
+
+sampling_rate = args.B
+wav_filepath = args.f
+
+
+#奥谷さんモジュール実行箇所
+
+
+#吉田は初めてで精度でないと思うので、切り離せるように
+if y_flg = True:
+    y_score =
+
+    #平均処理
+    ave_score = ({奥谷さんスコア} + y_score)/2
+
+    return ave_score
+
+return {奥谷さんスコア}
+
+#End
+
+
