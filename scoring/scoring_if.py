6d655db0b55e96d0168a5543ec21c96197460189
OKUYADaisuke <52092796+OKUYADaisuke@users.noreply.github.com>
2019-06-28 11:14:12 +0900

    Update scoring_if.py
    奥谷モジュールを呼び出せるように修正しました

--- scoring/scoring_if.py
+++ scoring/scoring_if.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import subprocess
 
 
 #parameter
@@ -18,20 +19,19 @@ args = parser.parse_args()
 beat = args.B
 wav_filepath = args.f
 
-
-#奥谷さんスコアリングモジュール実行箇所
-
+o_score_consoleoutput = subprocess.check_output(['Rscript', 'scoring_098.R', beat, wav_filepath], stderr=subprocess.DEVNULL)
+o_score = int(o_score_consoleoutput.split()[1])
 
 #吉田は初めてで精度でないと思うので、切り離せるように
 if y_flg = True:
-    y_score = (吉田スコアリングモジュール実行)
+    y_score = 50 #(吉田スコアリングモジュール実行)→暫定的に50を入れています
 
     #平均処理
-    ave_score = ({奥谷さんスコア} + y_score)/2
+    ave_score = (o_score + y_score)/2
 
     return ave_score
 
-return {奥谷さんスコア}
+return o_score
 
 #End
 
