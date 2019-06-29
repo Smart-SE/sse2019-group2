7f0167f03ebbf1f493cc04f635a5ed578a2b6f46
y2klink <y2klink@gmail.com>
2019-06-29 15:40:19 +0900

    Update y_module.py

--- scoring/y_module.py
+++ scoring/y_module.py
@@ -20,12 +20,52 @@ import re
 #For Data Augumentation
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+#param define
+kijun = 60
+sr_ef = 200
+width = 46
 
 
-def predict_score(b= 60 ,filepath = None)
+def load_wave_data(file_name):
+    file_path = os.path.join(WAV_DIR, file_name) #推論時はファイルパスまんま渡されるはず。
+    shs, fs = librosa.load(file_path, sr=samling_rate)
+    return shs,fs
 
+
+def calculate_melsp(x, n_fft=2048, hop_length=128):
+    stft = np.abs(librosa.stft(x, n_fft=n_fft, hop_length=hop_length))**2
+    log_stft = librosa.power_to_db(stft)
+    melsp = librosa.feature.melspectrogram(S=log_stft,n_mels=128)
+    return melsp
+
+def min_max(x, axis=None):
+    min = x.min(axis=axis, keepdims=True)
+    max = x.max(axis=axis, keepdims=True)
+    result = (x-min)/(max-min)
+    return result
+
+def predict_score(b= 120 ,filepath = None)
     score = None
 
+    samling_rate =  int(sr_ef *(tenpo/kijun))
+    
+    
+    X_pred = []
+    
+    try:
+        shs, fs = load_wave_data("100_flog_01.wav")
+        melsp = calculate_melsp(shs)
+        # 注意　
+        if melsp.shape[1] > width:
+            melsp_new = melsp[:,0:width]
+        else:
+            melsp_new = melsp 
+   
+        melsp_reg_ndary = min_max(melsp_new)
+        melsp_reg_ndary_reshape = np.expand_dims(melsp_reg_ndary, axis=-1)
+       
+        X_pred.append(melsp_reg_ndary_reshape)
+    
     try:
     
     
