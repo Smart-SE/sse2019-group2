a34fdd8e6496346d08dd8bb24e9c1a0beb9c0928
y2klink <y2klink@gmail.com>
2019-06-29 15:30:47 +0900

    Update y_module.py

--- scoring/y_module.py
+++ scoring/y_module.py
@@ -4,6 +4,23 @@
 import argparse
 import sys
 import traceback
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+import os
+import random
+import pandas as pd
+import librosa
+import librosa.display
+import re
+#For Data Augumentation
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
 
 def predict_score(b= 60 ,filepath = None)
 
