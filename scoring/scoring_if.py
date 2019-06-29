96b141699828387dd7fa9683f3e1bcd0d821d678
y2klink <y2klink@gmail.com>
2019-06-29 14:19:02 +0900

    Update scoring_if.py

--- scoring/scoring_if.py
+++ scoring/scoring_if.py
@@ -25,14 +25,19 @@ o_score = int(o_score_consoleoutput.split()[1])
 
 #吉田は初めてで精度でないと思うので、切り離せるように
 if y_flg = True:
-    y_score = 50 #(吉田スコアリングモジュール実行)→暫定的に50を入れています
-    # y_score = y_module.predict_score(b= beat,)
-    #平均処理
-    ave_score = (o_score + y_score)/2
-
-    return ave_score
-
-return o_score
+    #y_score = 50 #(吉田スコアリングモジュール実行)→暫定的に50を入れています =>Tureの時も値がちゃんと返るようにしました。
+    y_score = None
+    # y_score= y_module.predict_score(b= beat,filepath = wav_filepath)
+    #エラーハンドリング
+    #ちゃんと値が返ったら平均、無いなら奥谷さんスコアそのまま返す
+    if y_score != None:
+        #平均処理
+        ave_score = (o_score + y_score)/2
+        return ave_score
+    else:
+        return o_score
+else:
+    return o_score
 
 #End
 
