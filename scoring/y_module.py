4508bd9103711cfc7229ff8eb5a7b74f57c68bdd
y2klink <y2klink@gmail.com>
2019-06-29 14:57:56 +0900

    Create y_module.py

---
+++ scoring/y_module.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+
+def predict_score(b= 60 ,filepath = None)
+
+    score = None
+
+    try:
+    
+    
+    except:
+        print("ERROR")
+
+    finally:
+        return score
+
+
+if __name__ == '__main__'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-B')   
+    parser.add_argument('-f')
+    args = parser.parse_args()
+    beat = args.B
+    wav_filepath = args.f
+
+    return predict_score(b= beat,filepath = wav_filepath)
+
+#End
