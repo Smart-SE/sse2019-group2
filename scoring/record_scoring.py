f3ed5d8a789804043462e52dbca8a9610eee6c0d
tosshi1205 <t.miyagi.syuu@gmail.com>
2019-06-30 15:37:53 +0900

    add output file. add outputs directory

--- scoring/record_scoring.py
+++ scoring/record_scoring.py
@@ -1,7 +1,14 @@
+import csv
+import datetime
+
 import scoring_if
 import audio
 
 if __name__ == "__main__":
+    formated_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
     audio.record()
     score = scoring_if.scoring('./output.wav', '100')
-    print(score)
+
+    with open('/home/pi/sse2019-group2/outputs/result.csv', mode='a', newline='\n', encoding='utf-8') as fw:
+        writer = csv.writer(fw)
+        writer.writerow([formated_date, score])
