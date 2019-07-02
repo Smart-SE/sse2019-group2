c4043b0cf78421a0901309f4a8e50132a7c2ab1f
y2klink <y2klink@gmail.com>
2019-07-02 20:11:26 +0900

    Update scoring_if.py
    ケアレスミスの修正

--- scoring/scoring_if.py
+++ scoring/scoring_if.py
@@ -4,6 +4,7 @@
 import argparse
 import subprocess
 import y_module
+import sys
 
 
 #parameter
@@ -24,21 +25,19 @@ o_score_consoleoutput = subprocess.check_output(['Rscript', 'scoring_098.R', bea
 o_score = int(o_score_consoleoutput.split()[1])
 
 #吉田は初めてで精度でないと思うので、切り離せるように
-if y_flg = True:
+if y_flg == True:
     #y_score = 50 #(吉田スコアリングモジュール実行)→暫定的に50を入れています =>Tureの時も値がちゃんと返るようにしました。
     y_score = None
     # y_score= y_module.predict_score(b= beat,filepath = wav_filepath)
     #エラーハンドリング
     #ちゃんと値が返ったら平均、無いなら奥谷さんスコアそのまま返す
     if y_score != None:
-        #平均処理
-        ave_score = (o_score + y_score)/2
-        return ave_score
+        #平均処理 切り捨て
+        ave_score = int((o_score + y_score)/2)
+        exit(ave_score)
     else:
-        return o_score
+        exit(o_score)
 else:
-    return o_score
+    exit(o_score)
 
 #End
-
-
