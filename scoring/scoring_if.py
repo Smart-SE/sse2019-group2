539db3ba5eaa955e41dd58f6c010649799b8d2d9
y2klink <y2klink@gmail.com>
2019-07-04 18:23:24 +0900

    Add files via upload

--- scoring/scoring_if.py
+++ scoring/scoring_if.py
@@ -3,8 +3,8 @@
 
 import argparse
 import subprocess
-import y_module
 import sys
+import y_module
 
 
 #parameter
@@ -24,20 +24,19 @@ wav_filepath = args.f
 o_score_consoleoutput = subprocess.check_output(['Rscript', 'scoring_098.R', beat, wav_filepath], stderr=subprocess.DEVNULL)
 o_score = int(o_score_consoleoutput.split()[1])
 
-#吉田は初めてで精度でないと思うので、切り離せるように
 if y_flg == True:
     #y_score = 50 #(吉田スコアリングモジュール実行)→暫定的に50を入れています =>Tureの時も値がちゃんと返るようにしました。
     y_score = None
-    # y_score= y_module.predict_score(b= beat,filepath = wav_filepath)
+    y_score= y_module.predict_score(b = beat, file_path = wav_filepath)
     #エラーハンドリング
     #ちゃんと値が返ったら平均、無いなら奥谷さんスコアそのまま返す
+    #y_score = 30 
     if y_score != None:
-        #平均処理 切り捨て
+        #平均処理
         ave_score = int((o_score + y_score)/2)
         exit(ave_score)
     else:
         exit(o_score)
 else:
     exit(o_score)
-
 #End
