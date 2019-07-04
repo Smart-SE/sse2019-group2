af6f23db6bc436988e7d1f4f5321a91ff5d34ca0
y2klink <y2klink@gmail.com>
2019-07-04 18:31:46 +0900

    Add files via upload

--- scoring/y_module.py
+++ scoring/y_module.py
@@ -1,42 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import argparse
 import sys
+import os
 import traceback
+import argparse
+
 import numpy as np
 import pickle
-
 import keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
-import os
-import random
-import pandas as pd
 import librosa
-import librosa.display
-import re
-#For Data Augumentation
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 #param define
-kijun = 60
-sr_ef = 200
-width = 46
-modelfilepath = "/usr/bin/y_model.pickle"
+kijun = 100
+width = 800
+modelfilepath = "/root/smartSE/y_model.pickle"
+henkou_score = None
 
-def load_wave_data(file_name):
-    file_path = os.path.join(WAV_DIR, file_name) #推論時はファイルパスまんま渡されるはず。
-    shs, fs = librosa.load(file_path, sr=samling_rate)
-    return shs,fs
 
-def calculate_melsp(x, n_fft=2048, hop_length=128):
-    stft = np.abs(librosa.stft(x, n_fft=n_fft, hop_length=hop_length))**2
-    log_stft = librosa.power_to_db(stft)
-    melsp = librosa.feature.melspectrogram(S=log_stft,n_mels=128)
-    return melsp
+def load_wave_data(file_path):
+    music, fs = librosa.load(file_path)
+    return music,fs
 
 def min_max(x, axis=None):
     min = x.min(axis=axis, keepdims=True)
@@ -47,7 +35,7 @@ def min_max(x, axis=None):
 def rangecontrol(i):
     #print(i)
     if i > 100 :
-        ten = 100-(i - 100)
+        ten = 100 - (i - 100)
         return int(ten)
     elif i < 0 :
         ten = 0 + (-1 * i)
@@ -55,28 +43,30 @@ def rangecontrol(i):
     else:
       return int(i)
 
-def predict_score(b= 120 ,filepath = None)
-    score = None
-    samling_rate =  int(sr_ef *(tenpo/kijun))
+def predict_score(b = 100 ,file_path = None):
+    henkou_score = None
     X_pred = []   
+    
     try:
-        #モデルのロード
+        #model load
         with open(modelfilepath, 'rb') as f:
             model = pickle.load(f)
 
-        shs, fs = load_wave_data(filepath)
-        melsp = calculate_melsp(shs)
-        # 注意　widthは要調整
-        if melsp.shape[1] > width:
-            melsp_new = melsp[:,0:width]
+        # preparation
+        music, fs = load_wave_data(file_path)
+        CQ_spectro = np.abs(librosa.cqt(y=music, sr=fs, bins_per_octave=12*3, n_bins=7*12*3))
+        # width is magic number
+        if CQ_spectro.shape[1] > width:
+            CQ_spectro_new = CQ_spectro[:,0:width]
         else:
-            melsp_new = melsp 
-   
-        melsp_reg_ndary = min_max(melsp_new)
-        melsp_reg_ndary_reshape = np.expand_dims(melsp_reg_ndary, axis=-1)
-        X_pred.append(melsp_reg_ndary_reshape)
-        
-        pred= model.predict(X_pred)
+            CQ_spectro_new = CQ_spectro 
+
+        CQ_spectro_reg_ndary  =min_max(CQ_spectro_new)
+        CQ_spectro_ndary_reshape = np.expand_dims(CQ_spectro_reg_ndary, axis=-1)
+        X_pred.append(CQ_spectro_ndary_reshape)
+        X_pred_npary = np.array(X_pred)        
+        # predict
+        pred= model.predict(X_pred_npary)
         henkou_score = rangecontrol(pred[0])
         return henkou_score
     except:
@@ -85,10 +75,10 @@ def predict_score(b= 120 ,filepath = None)
         pass
     
     finally:
-        return score
+        return henkou_score
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-B')   
@@ -96,6 +86,6 @@ if __name__ == '__main__'
     args = parser.parse_args()
     beat = args.B
     wav_filepath = args.f
-    return predict_score(b= beat,filepath = wav_filepath)
-
+    y_score = predict_score(b= beat, file_path = wav_filepath)
+    exit(y_score)
 #End
