13b4d7fb898dd2196e59216183c148d57bf1deed
y2klink <y2klink@gmail.com>
2019-07-05 22:28:30 +0900

    Update scoring_if.py

--- scoring/scoring_if.py
+++ scoring/scoring_if.py
@@ -6,18 +6,14 @@ import subprocess
 import sys
 import y_module
 
-
 #parameter
 y_flg = True
 
-
 #main
 parser = argparse.ArgumentParser()
-
 parser.add_argument('-B')   
 parser.add_argument('-f')
 args = parser.parse_args()
-
 beat = args.B
 wav_filepath = args.f
 
@@ -27,7 +23,7 @@ o_score = int(o_score_consoleoutput.split()[1])
 if y_flg == True:
     #y_score = 50 #(吉田スコアリングモジュール実行)→暫定的に50を入れています =>Tureの時も値がちゃんと返るようにしました。
     y_score = None
-    y_score= y_module.predict_score(b = beat, file_path = wav_filepath)
+    y_score = y_module.predict_score(b = beat, file_path = wav_filepath)
     #エラーハンドリング
     #ちゃんと値が返ったら平均、無いなら奥谷さんスコアそのまま返す
     #y_score = 30 
