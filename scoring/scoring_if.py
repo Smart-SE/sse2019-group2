67b9ec2c4a9c826bb96a6db194cab2b93c89d7d1
y2klink <y2klink@gmail.com>
2019-07-06 19:30:07 +0900

    Update scoring_if.py

--- scoring/scoring_if.py
+++ scoring/scoring_if.py
@@ -7,25 +7,24 @@ import subprocess
 y_flg = True
 
 def scoring(wav_filepath: str, beat: str, y_flg=True):
-    o_score_consoleoutput = subprocess.check_output(
-        ['Rscript', 'scoring_098.R', beat, wav_filepath], stderr=subprocess.DEVNULL)
-    o_score = int(o_score_consoleoutput.split()[1])
-
-    # 吉田は初めてで精度でないと思うので、切り離せるように
     if y_flg:
-        # y_score = 50 #(吉田スコアリングモジュール実行)→暫定的に50を入れています =>Tureの時も値がちゃんと返るようにしました。
-        y_score = None
-        y_score = y_module.predict_score(b=beat, file_path=wav_filepath)
-        # エラーハンドリング
-        # ちゃんと値が返ったら平均、無いなら奥谷さんスコアそのまま返す
-        if y_score is not None:
-            # 平均処理
-            ave_score = (o_score + y_score)/2
-            return ave_score
-        else:
-            return o_score
-    else:
+        o_score_consoleoutput = subprocess.check_output(
+            ['Rscript', 'scoring_098.R', beat, wav_filepath], stderr=subprocess.DEVNULL)
+        o_score = int(o_score_consoleoutput.split()[1])
         return o_score
+    else:
+    # 回避策用
+        y_score = None
+        try:
+            y_score = y_module.predict_score(b=beat, file_path=wav_filepath)
+        except:
+            pass 
+        if y_score is not None:
+            # 平
+            return y_score
+        else:
+            #ランダマイズ
+            return y_score
 
 # End
 
